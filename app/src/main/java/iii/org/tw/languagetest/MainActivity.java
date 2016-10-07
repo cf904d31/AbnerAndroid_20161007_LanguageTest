9244f5a23fc04fb08521e04fc79c7ceed8314150
cf904d31 <cf904d31@gmail.com>
2016-10-07 15:51:49 +0800

    LanguageTest v2(增加改變ImageView及TextView)

--- app/src/main/java/iii/org/tw/languagetest/MainActivity.java
+++ app/src/main/java/iii/org/tw/languagetest/MainActivity.java
@@ -2,12 +2,24 @@ package iii.org.tw.languagetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textView;
+    private boolean isOn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView);
+        isOn = true;
+    }
+
+    public void test1(View v) {
+        isOn = !isOn;
+        textView.setText(isOn?R.string.text1_on:R.string.text1_off);
     }
 }
